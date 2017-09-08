6120ead5533b115223a27b52a4f6d99c2cd39c28
Adam Hynson <hynsondevelops@gmail.com>
2017-09-08 18:22:02 -0400

    Added index, added google maps API, popup window started

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,4 +3,8 @@ class ListingsController < ApplicationController
 	def show
 		@listing = Listing.find(params[:id])
 	end
+
+	def index
+		@listings = Listing.all
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,9 @@ City.create!(name: "Washington", state_id: 1)
 
 Zipcode.create!(name: 20500, state_id: 1)
 
+Zipcode.create!(name: 20018, state_id: 1)
+
 Listing.create!(address: "1600 Pennsylvania Ave NW", bedroom_count: "3", bathroom_count: "2.5", area_square_feet: 2000, description: "This is boiler plate for the description of this listing.", price: 150000, state_id: 1, city_id: 1, zipcode_id: 1)
 
+
+Listing.create!(address: "2135 Queens Chapel Rd NE", bedroom_count: "3", bathroom_count: "2.5", area_square_feet: 2000, description: "This is boiler plate for the description of this listing.", price: 150000, state_id: 1, city_id: 1, zipcode_id: 2)
