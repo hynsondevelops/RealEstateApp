f6c7ad196d17bf1a4ba5770a5003f0cd4388c4b7
Adam Hynson <hynsondevelops@gmail.com>
2017-10-06 23:16:42 -0400

    Added followed listings page.

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -4,6 +4,10 @@ class FollowingsController < ApplicationController
 		@following = Following.create(following_params)
 	end
 
+	def index
+		@followed_listings = current_user.followed_listings
+	end
+
 	private
 
 	  def following_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   has_many :listed, class_name: "Listing"
   has_many :followings
   has_many :followed_listings, through: :followings, :source => "listing"
+
+
+  def followed?(listing)
+  	print(followed_listings)
+  	followed_listings.each do |cur_listing|
+  		if (cur_listing.id == listing.id)
+  			return true;
+  		end
+  	end
+  	return false;
+  end
 end
+ 
